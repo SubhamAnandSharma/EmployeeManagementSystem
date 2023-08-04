fa0b19b2de51996e850b6e9133f363f830001bf6
Subham Sharma <100857437+SubhamAnandSharma@users.noreply.github.com>
2023-08-04 09:16:22 +0530

    Delete EMS directory

--- EMS/src/main/java/com/academia/repository/Attendance.java
+++
@@ -1,7 +0,0 @@
-package com.academia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Attendance extends JpaRepository<Attendance, Integer>{
-
-}
--- EMS/src/main/java/com/academia/repository/DepartmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.academia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.academia.entities.Department;
-
-public interface DepartmentRepo extends JpaRepository<Department, Integer>{
-
-}
